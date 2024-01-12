d2a61edc8191b476dd5637b574fc6bbc420de512
YeisonMC <yeisonmacalupu01@gmail.com>
2024-01-11 20:17:21 -0500

    Actualizacion en la base de datos

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ app.use(express.json());
 // TODO: USANDO RUTAS :D
 app.use("/rapimoney", routes);
 
-// app.get("/", (req, res) => {
-//   db.all("SELECT * FROM clientes", (err, rows) => {
-//     if (err) {
-//       console.error(err.message);
-//       return res.status(500).send("Error en la consulta a la base de datos");
-//     }
-//     res.json(rows);
-//   });
-// });
-
 const port = 3000;
 app.listen(port);
 console.log("Server running on port: " + port + " :D");
